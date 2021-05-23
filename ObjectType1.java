2587645d6945989fd0af678f16bfbd32baa4d772
SOURAV DESHMUKH <83661835+Sourav07code@users.noreply.github.com>
2021-05-23 23:39:18 +0530

    Create ObjectType1.java
    A sales person is paid commission based on the sales he makes as shown by the following
    table:
    
    SALES COMMISSION
    Under Rs. 500 2% of SALES
    Rs. 500 and under Rs. 5000 5% of SALES
    Rs. 5000 and over 8% of SALES
    
    Write a class, Commission, which has:
     An instance variable, sales; an appropriate constructor; and a method, getCommission()
    that returns the commission.
    Now write a Demo class in Java to test the Commission class by reading a sale from the
    user, using it to create a Commission object after validating that the value is not negative.
    Finally, call the getcommission() method to get and print the commission. If the sales are
    negative, your Demo class should print the message “Invalid Input”.

---
+++ ObjectType1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class comission{
+	double sales;
+	comission(double sales){
+		this.sales=sales;
+	}
+	double getComission() {
+		if(sales<500) return sales*0.02;
+		else if(sales>500 && sales<5000) return sales*0.05;
+		else return sales*0.08;
+	}
+}
+public class A2Q1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter sales ");
+		double sales=sc.nextDouble();
+		if(sales<0) System.out.println("Invalid input");
+		else {
+			comission c=new comission(sales);
+			double r=c.getComission();
+			System.out.println("Sales is "+sales+" and the comission is "+r);
+		}sc.close();
+	}
+
+}
