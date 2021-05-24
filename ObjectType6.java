8c6170b37728f86859a29f9d02ab1b2b14321218
SOURAV DESHMUKH <83661835+Sourav07code@users.noreply.github.com>
2021-05-24 23:43:12 +0530

    Create ObjectType6.java
     An abstract class Shape and the derived classes Square, Triangle and Circle to display area of different shapes.

---
+++ ObjectType6.java
@@ -0,0 +1,47 @@
+abstract class Shape{
+	abstract void area();
+}
+class Square extends Shape{
+	double side;
+	Square(double side){
+		this.side=side;
+	}
+	void area() {
+		double ar=side*side;
+		System.out.println(ar);
+	}
+}
+class Triangle extends Shape{
+	float base,height;
+	Triangle(float base,float height){
+		this.base=base;
+		this.height=height;
+	}
+	void area() {
+		double ar=0.5*base*height;
+		System.out.println(ar);
+	}
+}
+class Circle extends Shape{
+	double r;
+	Circle(double r){
+		this.r=r;
+	}
+	void area() {
+		double ar=Math.PI*r*r;
+		System.out.println(ar);
+	}
+}
+public class Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape s=new Square(4);
+		s.area();
+		Shape t=new Triangle(3,5);
+		t.area();
+		Shape c=new Circle(5);
+		c.area();
+	}
+
+}
